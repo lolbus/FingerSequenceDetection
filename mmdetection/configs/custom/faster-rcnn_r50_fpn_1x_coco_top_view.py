65293bca3adffadfa2dc78401d2db22097c28971
lolbus <guangway@hotmail.com>
2023-08-29 15:53:48 +0800

    add mmdetection source code

---
+++ mmdetection/configs/custom/faster-rcnn_r50_fpn_1x_coco_top_view.py
@@ -0,0 +1,5 @@
+_base_ = [
+    '../_base_/models/faster-rcnn_r50_fp_dda_top_view.py',
+    '../_base_/datasets/coco_detection_top_view.py',
+    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
---
+++ mmdetection/mmdet/configs/custom/faster-rcnn_r50_fpn_1x_coco.py
@@ -0,0 +1,5 @@
+_base_ = [
+    'C:/Users/ASUS/Desktop/coco/mmdetection/configs/_base_/models/faster-rcnn_r50_fpn.py',
+    'C:/Users/ASUS/Desktop/coco/mmdetection/configs/_base_/datasets/coco_detection.py',
+    'C:/Users/ASUS/Desktop/coco/mmdetection/configs/_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
+]
